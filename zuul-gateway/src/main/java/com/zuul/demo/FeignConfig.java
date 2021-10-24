3077b2b8edfc0ef8670dc0e79a2e4a01b3e27643
moete <moetez.boubakri@esprit.tn>
2021-10-24 23:31:14 +0200

    add zuul-gateway + config gateway

---
+++ zuul-gateway/src/main/java/com/zuul/demo/FeignConfig.java
@@ -0,0 +1,16 @@
+package com.zuul.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor(){
+        return  new BasicAuthRequestInterceptor("user", "password");
+    }
+
+}
---
+++ zuul-gateway/src/main/java/com/zuul/demo/ZuulGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.zuul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableFeignClients("com.zuul.demo")
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
